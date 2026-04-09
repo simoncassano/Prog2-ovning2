d3c111b4549e73d8dd65742b91db0bceee7e555c
simoncassano <simoncassano@hotmail.se>
2026-04-09 17:45:46 +0200

    formatted

--- src/main/java/se/su/ovning2/Searcher.java
+++ src/main/java/se/su/ovning2/Searcher.java
@@ -7,90 +7,101 @@ import java.util.SortedSet;
 
 public class Searcher implements SearchOperations {
 
-  private Set<String> genres;
-  private Set<String> artists;
-  private Set<String> titles;
-  private Collection<Recording> recordings;
+    private Set<String> genres;
+    private Set<String> artists;
+    private Set<String> titles;
+    private Collection<Recording> recordings;
 // ^ instansvariabler ^
 
-  public Searcher(Collection<Recording> data) {
-    recordings = data;
+    public Searcher(Collection<Recording> data) {
+        recordings = data;
 
-    genres = new HashSet<>();
-    artists = new HashSet<>();
-    titles = new HashSet<>();
+        genres = new HashSet<>();
+        artists = new HashSet<>();
+        titles = new HashSet<>();
 
-    for (Recording r : data){
-      artists.add(r.getArtist());
-      titles.add(r.getTitle());
-      genres.addAll(r.getGenre());
-    }/*For loop som lägger till varje artist från varje recording i en hash set och gör samma sak för
+        for (Recording r : data) {
+            artists.add(r.getArtist());
+            titles.add(r.getTitle());
+            genres.addAll(r.getGenre());
+        }/*For loop som lägger till varje artist från varje recording i en hash set och gör samma sak för
     titles och genrer.*/
-  }
-    //simon
-  @Override
-  public long numberOfArtists() {
-    return artists.size();
+    }
 
-  }
-    //antonios
-  @Override
-  public long numberOfGenres() {
-    return genres.size();
-  }
     //simon
-  @Override
-  public long numberOfTitles() {
-    return titles.size();
-  }
-  //antonios
-  @Override
-  public boolean doesArtistExist(String name) {
-    // TODO Auto-generated method stub
-    return artists.contains(name);
-  }
-    //simon
-  @Override
-  public Collection<String> getGenres() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getGenres'");
-  }
-    //antonios
-  @Override
-  public Recording getRecordingByName(String title) {
-    // TODO Auto-generated method stub
+    @Override
+    public long numberOfArtists() {
+        return artists.size();
 
-    throw new UnsupportedOperationException("Unimplemented method 'getRecordingByName'");
-  }
-    //simon
-  @Override
-  public Collection<Recording> getRecordingsAfter(int year) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getRecordingsAfter'");
-  }
+    }
+
     //antonios
-  @Override
-  public SortedSet<Recording> getRecordingsByArtistOrderedByYearAsc(String artist) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException(
-        "Unimplemented method 'getRecordingsByArtistOrderedByYearAsc'");
-  }
+    @Override
+    public long numberOfGenres() {
+        return genres.size();
+    }
+
     //simon
-  @Override
-  public Collection<Recording> getRecordingsByGenre(String genre) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getRecordingsByGenre'");
-  }
+    @Override
+    public long numberOfTitles() {
+        return titles.size();
+    }
+
     //antonios
-  @Override
-  public Collection<Recording> getRecordingsByGenreAndYear(String genre, int yearFrom, int yearTo) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getRecordingsByGenreAndYear'");
-  }
+    @Override
+    public boolean doesArtistExist(String name) {
+        // TODO Auto-generated method stub
+        return artists.contains(name);
+    }
+
     //simon
-  @Override
-  public Collection<Recording> offerHasNewRecordings(Collection<Recording> offered) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'offerHasNewRecordings'");
-  }
+    @Override
+    public Collection<String> getGenres() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGenres'");
+    }
+
+    //antonios
+    @Override
+    public Recording getRecordingByName(String title) {
+        // TODO Auto-generated method stub
+
+        throw new UnsupportedOperationException("Unimplemented method 'getRecordingByName'");
+    }
+
+    //simon
+    @Override
+    public Collection<Recording> getRecordingsAfter(int year) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRecordingsAfter'");
+    }
+
+    //antonios
+    @Override
+    public SortedSet<Recording> getRecordingsByArtistOrderedByYearAsc(String artist) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+                "Unimplemented method 'getRecordingsByArtistOrderedByYearAsc'");
+    }
+
+    //simon
+    @Override
+    public Collection<Recording> getRecordingsByGenre(String genre) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRecordingsByGenre'");
+    }
+
+    //antonios
+    @Override
+    public Collection<Recording> getRecordingsByGenreAndYear(String genre, int yearFrom, int yearTo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRecordingsByGenreAndYear'");
+    }
+
+    //simon
+    @Override
+    public Collection<Recording> offerHasNewRecordings(Collection<Recording> offered) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'offerHasNewRecordings'");
+    }
 }
