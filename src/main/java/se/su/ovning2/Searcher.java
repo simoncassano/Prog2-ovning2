a1c327ac24f5d3cd3adb169cdd04cc81eb581ef1
simoncassano <simoncassano@hotmail.se>
2026-04-10 15:00:43 +0200

    fixad

--- src/main/java/se/su/ovning2/Searcher.java
+++ src/main/java/se/su/ovning2/Searcher.java
@@ -4,20 +4,11 @@ import java.util.*;
 
 public class Searcher implements SearchOperations {
 
-<<<<<<< HEAD
-  private Set<String> genres;
-  private Set<String> artists;
-  private Set<String> titles;
-  private Collection<Recording> recordings;
-  private Map<String, Recording> recordingsByTitle;
-=======
-
-
     private Set<String> genres;
     private Set<String> artists;
     private Set<String> titles;
     private Collection<Recording> recordings;
->>>>>>> bd757dcb43d69d23c895a64785b8ee0e40a91283
+    private Map<String, Recording> recordingsByTitle;
 // ^ instansvariabler ^
 
     public Searcher(Collection<Recording> data) {
@@ -33,74 +24,26 @@ public class Searcher implements SearchOperations {
             genres.addAll(r.getGenre());
         }/*For loop som lägger till varje artist från varje recording i en hash set och gör samma sak för
     titles och genrer.*/
-<<<<<<< HEAD
 
-    recordingsByTitle = new HashMap<>();
-    for (Recording r: data){
-      recordingsByTitle.put(r.getTitle(), r);
-    }/*Sparar varje recording i en map med titel som nyckel*/
+        recordingsByTitle = new HashMap<>();
+        for (Recording r : data) {
+            recordingsByTitle.put(r.getTitle(), r);
+        }/*Sparar varje recording i en map med titel som nyckel*/
 
-  }
-    //simon
-  @Override
-  public long numberOfArtists() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'numberOfArtists'");
-  }
-    //antonios
-  @Override
-  public long numberOfGenres() {
-    return genres.size();
-  }
-    //simon
-  @Override
-  public long numberOfTitles() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'numberOfTitles'");
-  }
-  //antonios
-  @Override
-  public boolean doesArtistExist(String name) {
-    return artists.contains(name);
-  }
-    //simon
-  @Override
-  public Collection<String> getGenres() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getGenres'");
-  }
-    //antonios
-  @Override
-  public Recording getRecordingByName(String title) {
-    return recordingsByTitle.get(title);
-    //Hämtar recordingen som har parameterns titel från hashmapen recordingsByTitle.
-  }
-=======
     }
 
->>>>>>> bd757dcb43d69d23c895a64785b8ee0e40a91283
     //simon
     @Override
     public long numberOfArtists() {
         return artists.size();
-
     }
 
     //antonios
-<<<<<<< HEAD
-  @Override
-  public SortedSet<Recording> getRecordingsByArtistOrderedByYearAsc(String artist) {
-      
-    throw new UnsupportedOperationException(
-        "Unimplemented method 'getRecordingsByArtistOrderedByYearAsc'");
-  }
-=======
     @Override
     public long numberOfGenres() {
         return genres.size();
     }
 
->>>>>>> bd757dcb43d69d23c895a64785b8ee0e40a91283
     //simon
     @Override
     public long numberOfTitles() {
@@ -110,23 +53,21 @@ public class Searcher implements SearchOperations {
     //antonios
     @Override
     public boolean doesArtistExist(String name) {
-        // TODO Auto-generated method stub
         return artists.contains(name);
     }
 
     //simon
     @Override
     public Collection<String> getGenres() {
-        return Collections.unmodifiableSet(genres);
-
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGenres'");
     }
 
     //antonios
     @Override
     public Recording getRecordingByName(String title) {
-        // TODO Auto-generated method stub
-
-        throw new UnsupportedOperationException("Unimplemented method 'getRecordingByName'");
+        return recordingsByTitle.get(title);
+        //Hämtar recordingen som har parameterns titel från hashmapen recordingsByTitle.
     }
 
     //simon
@@ -139,7 +80,7 @@ public class Searcher implements SearchOperations {
     //antonios
     @Override
     public SortedSet<Recording> getRecordingsByArtistOrderedByYearAsc(String artist) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException(
                 "Unimplemented method 'getRecordingsByArtistOrderedByYearAsc'");
     }
@@ -164,4 +105,6 @@ public class Searcher implements SearchOperations {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'offerHasNewRecordings'");
     }
+
+
 }
