b164cd1bf6c7aafbf0b4ab69c3dfa2d382a64870
Rasmus <rjurss@gmail.com>
2026-04-12 16:39:56 +0200

    La till nödvändiga delar för hashmap och träd, var dock rätt så klurig så hoppas jag gjorde rätt

--- src/main/java/se/su/ovning2/Recording.java
+++ src/main/java/se/su/ovning2/Recording.java
@@ -1,9 +1,8 @@
 package se.su.ovning2;
 
-import java.util.Collection;
-import java.util.Set;
+import java.util.*;
 
-public class Recording {
+public class Recording implements Comparable<Recording> {
   private final int year;
   private final String artist;
   private final String title;
@@ -37,6 +36,47 @@ public class Recording {
   public int getYear() {
     return year;
   }
+
+  /*Är en Override av Javas compareTo metod med egen kod för detta sammahanget
+  *Vi använder denna för att sortera objekten till vår TreeMap/Set
+  * Om compare != 0 så avslutar vi metoden tidigt då vi vet att
+  * Någon variabel inte matchar
+  * */
+
+  @Override
+  public int compareTo(Recording toBeCompared) {
+    int compare = this.title.compareTo(toBeCompared.title);
+    if (compare != 0) {
+      return compare;
+    }
+    compare = this.artist.compareTo(toBeCompared.artist);
+    if (compare !=0) {
+      return compare;
+    }
+    return Integer.compare(this.year, toBeCompared.year);
+  }
+
+  /*Är vår version av javas equals metod, använder artist, år och titel
+  som unik kombination
+  *
+  * */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (!(obj instanceof Recording)) return false;
+    Recording toBeCompared = (Recording) obj;
+    return year == toBeCompared.year && artist.equals(toBeCompared.artist) && title.equals(toBeCompared.title);
+  }
+
+  /*Javas metod hashCode fast med våra variabler
+  *
+  * */
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(year, artist, title);
+  }
+
 
   @Override
   public String toString() {
